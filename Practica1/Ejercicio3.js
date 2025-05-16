a88030283572ebb26b85e930724448c0e0169ee4
IsaacGomez9628 <122044188@upqedu.onmicrosoft.com>
2025-05-16 15:26:40 -0600

    Ejercicio 3
    Funcion de flecha

---
+++ Practica1/Ejercicio3.js
@@ -0,0 +1,9 @@
+/*
+Crea una arrow function llamada saludoPersonalizado para reciba dos parametros: nombre y edad, y retorne una cadena como la siguiente
+*/
+
+const saludoPersonalizado = (nombre, edad) => {
+  return `Hola ${nombre}, tienes ${edad} años.`;
+};
+
+console.log(saludoPersonalizado("Isaac", 25));

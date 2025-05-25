6a1f3b2a8d83e7641897d126d6ae888e2ddec2f3
IsaacGomez9628 <122044188@upqedu.onmicrosoft.com>
2025-05-25 13:18:01 -0600

    Practica 3 Ejercicio 1

---
+++ Practica3/Ejercicio1/main.js
@@ -0,0 +1,6 @@
+import { restar } from "./utils.mjs";
+console.log(restar(10, 5));
+console.log(restar(50, 15));
+console.log(restar(80, 25));
+console.log(restar(100, 35));
+console.log(restar(200, 45));
